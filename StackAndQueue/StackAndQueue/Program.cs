8887ce1d12e20790848cbf57f24f09873edbd7e1
sanajylopis25 <sanajylopis25@gmail.com>
2021-12-21 23:41:42 +0530

    First Commit

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            linkedListStack.Peek(); 
+            linkedListStack.Pop();
+            linkedListStack.IsEmpty();
+            linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
